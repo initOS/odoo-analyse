19ad1e502ac3385bed2d6425da861d4a5bb45826
ahashaam <ali.hashaam@initos.com>
2020-04-27 13:30:00 +0200

    version number updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="odoo-analyse",
-    version="0.1.0",
+    version="0.1.1",
     author="initOS GmbH",
     author_email="info@initos.com",
     description="Package to analyse odoo modules",
