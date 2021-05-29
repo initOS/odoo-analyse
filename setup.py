d71806d6547d2dfb395361463f6eb838a3cdbe95
fkantelberg <florian.kantelberg@initos.com>
2021-05-30 00:09:07 +0200

    Bump minor version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="odoo-analyse",
-    version="1.0.1",
+    version="1.1",
     author="initOS GmbH",
     author_email="info@initos.com",
     description="Package to analyse odoo modules",
